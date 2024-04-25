7934fc8132ed7a0a2332e9ed200e109ecb3abadd
RyuaUehara <u-ryua-2410@occ.co.jp>
2024-04-25 17:56:40 +0900

    omg

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,112 +2,75 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="antialiased overflow-x-hidden">
+      <div className="bg-white min-h-screen pt-8 px-4 lg:px-0">
+        <title>TailwindTemplates v2 is here - Tailwindtemplates</title>
+        <div className="container mx-auto flex flex-col lg:flex-row items-start justify-between">
+          <div className="flex items-center ml-6 mb-8 lg:mb-0">
+            <Image src="/images/rogo.jpeg" alt="Logo" width={60} height={60} layout="fixed" />
+            <h1 className="text-2xl ml-4 font-semibold">TailwindTemplates</h1>
+          </div>
+          <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
+            <nav className="ml-auto mr-20 flex space-x-6 lg:space-x-10">
+              <a href="#" className="text-gray-900 hover:text-blue-600 transition duration-300 px-8 py-2">Templates</a>
+              <a href="#" className="text-gray-900 hover:text-blue-600 transition duration-300 px-8 py-2">Resources</a>
+              <a href="#" className="text-gray-900 hover:text-blue-600 transition duration-300 px-8 py-2">Blog</a>
+              <a href="#" className="text-gray-900 hover:text-blue-600 transition duration-300 px-8 py-2">About</a>
+            </nav>
+          </div>
+        </div>
+        <div className="h-96 w-full bg-gray-100 mb-24 mt-24 rounded-2xl shadow-xl bg-cover bg-center">
+
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg p-4 max-w-5xl">
+            <a href="#" className="bg-blue-100 text-blue-400 font-bold py-2 px-4 rounded-full">Update</a><br /><br /><br />
+            <h1 className="text-5xl mb-4">TailwindTemplates v2 is here</h1><br />
+            <p className="text-1xl text-gray-500">1 min read</p><br /><br />
+            <p className="text-1xl text-gray-500">TailwindTemplates V2 is finally here! Our focus for TailwindTemplates v2 is to bring open-source design systems to Tailwindcss. Each design system has a detailed guideline to provide consistency and coherence within a user interface.</p><br />
+            <h2 className="text-2xl mb-4">What to expect?</h2>
+            <p className="text-1xl text-gray-500">Over the coming months, we will expand the design systems that are currently available at launch. In addition to that, we are planning to create several tailwind configurations to easily get started with the proper colors and variants that belong to each design system. </p><br />
+            <h2 className="text-2xl mb-4">Not only templates</h2>
+            <p className="text-1xl text-gray-500 mb-24">We are planning to bring more tailwind and web development-related content such as videos, blog posts, tutorials and news.</p>
+          </div>
+        </div>
+        <div className="bg-blue-700 text-white flex justify-between px-6 pt-1 pb-1">
+          <div className="mx-auto container px-6 pt-24 pb-16">
+            <div className="grid lg:grid-cols-3 px-6 lg:px-0 gap-4">
+              <div className="container mx-auto">
+                <h1>About</h1>
+                <ul>
+                  <li><a href="#" className="text-white hover:text-yellow-500">Me</a></li>
+                  <li><a href="#" className="text-white hover:text-yellow-500">Contact</a></li>
+                  <li><a href="#" className="text-white hover:text-yellow-500">Disclaimer</a></li>
+                </ul>
+              </div>
+              <div className="container mx-auto">
+                <h1>Resources</h1>
+                <ul>
+                  <li><a href="#" className="text-white hover:text-yellow-500">Amazing</a></li>
+                  <li><a href="#" className="text-white hover:text-yellow-500">Blog</a></li>
+                </ul>
+              </div>
+              <div className="mt-4 inline-block">
+                <a href="#" className="bg-pink-500 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded">Subscribe to newsletter</a>
+              </div>
+            </div>
+            <div className="flex lg:justify-center px-6 lg:px-0 items-center mt-12 mb-6">
+              <div className="mt-20 inline-block">
+                <a href="#" className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">Support Me on Ko-fi</a>
+              </div>
+            </div>
+            <div className="flex lg:justify-center">
+              <p>
+                Built with TALL stack &
+                <a href="#" className="hover:text-red-500"> ❤ </a>
+                by j-hiz
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
